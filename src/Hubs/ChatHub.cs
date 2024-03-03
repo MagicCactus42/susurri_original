a37b2957a880d33ae522e539a1e5c925c879afa1
MagicCactus42 <magiccactus42@proton.me>
2024-03-03 13:58:38 +0100

    added solution file

---
+++ src/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Susurri.Hubs;
+
+public class ChatHub : Hub
+{
+    public Task SendMessage(string user, string message)
+    {
+        return Clients.All.SendAsync("ReceiveMessage", user, message);
+    }
+}
